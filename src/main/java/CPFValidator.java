0dd765c81d6c6b37a3d94201f58795f795a46033
JPLabussiereF <labussework@gmail.com>
2025-06-07 11:32:20 -0300

    add calculateCheckDigits() in CPFValidator.java

--- src/main/java/CPFValidator.java
+++ src/main/java/CPFValidator.java
@@ -2,19 +2,56 @@ public class CPFValidator {
     String name;
     String cpf;
 
-    public CPFValidator(String name, String cpf){
+    public CPFValidator(String name, String cpf) {
         this.name = name;
         this.cpf = cpf;
     }
 
-    public void isLengthValid(){
-        if (cpf.length() != 11){
+    public void isLengthValid() {
+        if (cpf.length() != 11) {
             System.out.println("Digite um CPF de 11 digitos");
             return;
         }
         System.out.printf("O seu cpf '%s' possui 11 digitos", cpf);
     }
 
+    public void calculateCheckDigits() {
+        int sumFirstDigit = 0;
+        int sumSecondDigit = 0;
 
+        for (int i = 0; i < 9; i++) {
+            int aux = Character.getNumericValue(cpf.charAt(i));
+            sumFirstDigit += aux * (10 - i);
+        }
 
+        sumFirstDigit %= 11;
+        int valueFirstDigit;
+
+        if (sumFirstDigit < 2) {
+            valueFirstDigit = 0;
+        } else {
+            valueFirstDigit = 11 - sumFirstDigit;
+        }
+
+        for (int i = 0; i < 10; i++) {
+            int aux = Character.getNumericValue(cpf.charAt(i));
+            sumSecondDigit += aux * (11 - i);
+        }
+
+        sumSecondDigit %= 11;
+        int valueSecondDigit;
+
+        if (sumSecondDigit <2) {
+            valueSecondDigit = 0;
+        } else {
+            valueSecondDigit = 11 - sumSecondDigit;
+        }
+
+        if (valueFirstDigit == Character.getNumericValue(cpf.charAt(9)) && valueSecondDigit == Character.getNumericValue(cpf.charAt(10))) {
+            System.out.println("Seu CPF é válido!");
+        } else {
+            System.out.println("Seu CPF é inválido!");
+        }
+
+    }
 }
--- src/main/java/main.java
+++ src/main/java/main.java
@@ -1,27 +1,31 @@
 public static void main(String[] args) {
 
-    Rectangle rectangle = new Rectangle(7.0,10.0);
-    System.out.printf("A base do retângulo é: %f\n", rectangle.base);
-    System.out.printf("A altura do retângulo é: %f\n", rectangle.height);
-    System.out.printf("A área do retângulo é: %f\n", rectangle.calculateArea());
-    System.out.printf("O perímetro do retângulo é: %f\n", rectangle.calculatePerimeter());
-    System.out.printf("O retângulo é quadrado?: %b\n", rectangle.isSquare());
+//    Rectangle rectangle = new Rectangle(7.0,10.0);
+//    System.out.printf("A base do retângulo é: %f\n", rectangle.base);
+//    System.out.printf("A altura do retângulo é: %f\n", rectangle.height);
+//    System.out.printf("A área do retângulo é: %f\n", rectangle.calculateArea());
+//    System.out.printf("O perímetro do retângulo é: %f\n", rectangle.calculatePerimeter());
+//    System.out.printf("O retângulo é quadrado?: %b\n", rectangle.isSquare());
+//
+//    System.out.println();
+//
+//    Product p1 = new Product("Mouse", 90);
+//    p1.showInfo();
+//
+//    Product p2 = new Product("Teclado", 250);
+//    p2.showInfo();
+//
+//    p1.compareProducts(p2);
+//    p2.compareProducts(p1);
+//
+//    p1.applyDiscount(20.1);
+//    p2.applyDiscount(-1);
+//
+//    p1.calculateTax(1001);
+//    p2.calculateTax(100);
 
-    System.out.println();
+    CPFValidator cpf1 =new CPFValidator("julio", "86588340508");
+    cpf1.calculateCheckDigits();
 
-    Product p1 = new Product("Mouse", 90);
-    p1.showInfo();
-
-    Product p2 = new Product("Teclado", 250);
-    p2.showInfo();
-
-    p1.compareProducts(p2);
-    p2.compareProducts(p1);
-
-    p1.applyDiscount(20.1);
-    p2.applyDiscount(-1);
-
-    p1.calculateTax(1001);
-    p2.calculateTax(100);
 
 }
